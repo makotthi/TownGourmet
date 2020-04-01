3a42be4ad79224cf25429d2a47158da69848fd3c
makotthi <gurifonnkatou@gmail.com>
2020-04-02 00:34:05 +0900

    FavoritesViewController.swiftを追加

---
+++ TownGourmet/TownGourmet/Controllers/FavoritesViewController.swift
@@ -0,0 +1,14 @@
+
+import UIKit
+
+class FavoritesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
