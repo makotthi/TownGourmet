5224aa176a68c2de85e6392ee8aed3de141af7f3
makotthi <gurifonnkatou@gmail.com>
2020-04-03 01:55:45 +0900

    SearchViewController.swiftにprivateを追加

--- TownGourmet/TownGourmet/Controllers/SearchViewController.swift
+++ TownGourmet/TownGourmet/Controllers/SearchViewController.swift
@@ -4,9 +4,9 @@ import UIKit
 class SearchViewController: UIViewController {
 
     // 現在地を再取得ボタン
-    @IBOutlet weak var reloadCurrentLocationButton: UIButton!
+    @IBOutlet private weak var reloadCurrentLocationButton: UIButton!
     // 現在地周辺のレストランを検索ボタン
-    @IBOutlet weak var searchRestaurantsAroundButton: UIButton!
+    @IBOutlet private weak var searchRestaurantsAroundButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,10 +22,10 @@ class SearchViewController: UIViewController {
 extension SearchViewController {
     
     // 現在地再取得ボタンが押された時の処理
-    @IBAction func reloadCurrentLocation(_ sender: Any) {
+    @IBAction private func reloadCurrentLocation(_ sender: Any) {
     }
     
     // 現在地周辺のレストランを検索ボタンが押された時の処理
-    @IBAction func searchRestaurantsAround(_ sender: Any) {
+    @IBAction private func searchRestaurantsAround(_ sender: Any) {
     }
 }
